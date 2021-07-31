3613d06833b2f420a79fbdbcaa7a7872cd2ba2b7
Hippoey <75302230+Hippoey@users.noreply.github.com>
2021-07-31 17:04:45 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,21 +12,22 @@ client = discord.Client()
 
 @client.event 
 async def on_ready():
-    print('Bot is ready.')
+    print('Bot is ready .')
 
 @client.event
 async def on_message(message: discord.Message):
     if message.author.bot:
         return
-    if message.content == "huh": 
+    if "huh" in message.content: 
         await message.channel.send("HUH")
-
-@client.event
-async def on_message(message:discord.Message):
-    if message.author.bot:
-        return
-    if message.content == "Hi":
-        await message.channel.send("Hi bubba")
+    if message.content == "hi":
+        await message.channel.send("Hello")
+    if message.content == "sheesh":
+        await message.channel.send("<@665488298533322762>", delete_after=0.5)
+        await message.delete()
+    if message.content =='luminor':
+        embedVar = discord.Embed(title="Hi! I'm Luminor Bot", description="", color=0x6c5ce7)
+        await message.channel.send(embed=embedVar)
 
 keep_alive() 
 client.run(TOKEN)
