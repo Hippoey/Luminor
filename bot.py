1d7648b93e8f214ac602220055bd766292d13c27
Hippoey <75302230+Hippoey@users.noreply.github.com>
2021-08-28 11:43:41 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,14 +22,14 @@ reddit = praw.Reddit(client_id='Dec7VPO9YEnNZ2vK_Bhu1w',
 @tasks.loop(minutes=20)
 async def test():
     channel = client.get_channel(868076426312704021)
-    message = ["What's that?", "You got that drip? Dayum bro!", "Hello there","HUH", "Sheeeeeeeesh!!!!!!"]
+    message = ["What's that?", "You got that drip? Dayum bro!", "Hello there","HUH", "Sheeeeeeeesh!!!!!!", "Baba Boeey", "That's cap", "baaaap"]
     await channel.send(message[random. randint(0,3)])
 
 @client.event 
 async def on_ready():
    activity = discord.Game(name=("luminor help"), type=3)
    test.start()
-   await client.change_presence(status=discord.Status.idle, activity=activity)
+   await client.change_presence(status=discord.Status.online, activity=activity)
    print('Bot is ready .')
 
 @client.event
